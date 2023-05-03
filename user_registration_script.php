fda8b3f1db32bd631841b9d472f46d2db73412c4
Ermek <ermek.ilikeshova_2025@ucentralasia.org>
2023-05-03 19:23:31 +0600

    login and sign up

---
+++ user_registration_script.php
@@ -0,0 +1,42 @@
+<?php
+    require 'connection.php';
+    session_start();
+    $name= mysqli_real_escape_string($con,$_POST['name']);
+    $email=mysqli_real_escape_string($con,$_POST['email']);
+    $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
+    if(!preg_match($regex_email,$email)){
+        echo "Incorrect email. Redirecting you back to registration page...";
+        ?>
+        <meta http-equiv="refresh" content="2;url=signup.php" />
+        <?php
+    }
+    $password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
+    if(strlen($password)<6){
+        echo "Password should have atleast 5 characters. Redirecting you back to registration page...";
+        ?>
+        <meta http-equiv="refresh" content="2;url=signup.php" />
+        <?php
+    }
+$year=$_POST['year'];
+    $duplicate_user_query="select id from users where email='$email'";
+    $duplicate_user_result=mysqli_query($con,$duplicate_user_query) or die(mysqli_error($con));
+    $rows_fetched=mysqli_num_rows($duplicate_user_result);
+    if($rows_fetched>0){
+        ?>
+        <script>
+            window.alert("Email already exists in our database!");
+        </script>
+        <meta http-equiv="refresh" content="1;url=signup.php" />
+        <?php
+    }else{
+        $user_registration_query="insert into users(name,email,year,password) values ('$name','$email','$year','$password')";
+        $user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
+        echo "User successfully registered";
+        $_SESSION['email']=$email;
+        $_SESSION['id']=mysqli_insert_id($con); 
+        ?>
+        <meta http-equiv="refresh" content="3;url=index.php" />
+        <?php
+    }
+    
+?>
